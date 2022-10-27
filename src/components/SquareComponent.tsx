9153ca35881391e038bd5f95d56065bda6bda472
Kevin Constantin <kconstantin@protonmail.com>
2022-10-27 12:56:18 -0700

    added black/white piece icons

--- src/components/SquareComponent.tsx
+++ src/components/SquareComponent.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import Piece from '../classes/Piece';
+import { 
+  GiChessPawn, 
+  GiChessRook, 
+  GiChessKnight, 
+  GiChessBishop, 
+  GiChessQueen, 
+  GiChessKing 
+} from "react-icons/gi";
 
 interface SquareComponentProps {
   className: string;
@@ -8,10 +16,45 @@ interface SquareComponentProps {
 }
 
 export default function SquareComponent({ className, piece, name }: SquareComponentProps) {
+  let pieceIcon = null;
+  let iconStyle = '';
+
+  if (piece?.color === 'black') {
+    iconStyle = 'piece--black';
+  } else if (piece?.color === 'white') {
+    iconStyle = 'piece--white';
+  }
+
+  if (piece) {
+    switch (piece.type) {
+      case 'pawn':
+        pieceIcon = <GiChessPawn className={iconStyle} />
+        break;
+      case 'bishop':
+        pieceIcon = <GiChessBishop className={iconStyle} />
+        break;
+      case 'knight':
+        pieceIcon = <GiChessKnight className={iconStyle} />
+        break;
+      case 'rook':
+        pieceIcon = <GiChessRook className={iconStyle} />
+        break;
+      case 'queen':
+        pieceIcon = <GiChessQueen className={iconStyle} />
+        break;
+      case 'king':
+        pieceIcon = <GiChessKing className={iconStyle} />
+        break;
+    }
+  }
+  
 
   return (
     <div className={className}>
-      {name}
+      <div>{name}</div>
+      <div className="piece-icon">
+        {piece !== null && pieceIcon}
+      </div>
     </div>
   )
 }
